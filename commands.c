e7898c85e761084e7d664850afac0ae64edba5dc
Bruce Hill <bruce@bruce-hill.com>
2025-08-25 00:12:44 -0400

    Clean up some unistr.h issues and expand some parse macros.

--- commands.c
+++ commands.c
@@ -12,7 +12,17 @@
 #include <sys/param.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <unistr.h>
+
+// This is a workaround fix for an issue on some systems that don't have `__GLIBC__` defined
+// and run into problems with <unistr.h>
+
+#ifndef __GLIBC__
+#define __GLIBC__ 2
+#include <unistr.h> // IWYU pragma: export
+#undef __GLIBC__
+#else
+#include <unistr.h> // IWYU pragma: export
+#endif
 
 #define READ_END 0
 #define WRITE_END 1
